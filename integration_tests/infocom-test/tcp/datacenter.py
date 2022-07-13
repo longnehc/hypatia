99a2e26c7fab03da5020fd706f7d4a099f28d945
Yi Ching Chou (David) <mrycchou@gmail.com>
2022-07-12 17:06:30 -0700

    Add datacenter script file

---
+++ integration_tests/infocom-test/tcp/datacenter.py
@@ -0,0 +1,16 @@
+DATACENTERS = [
+    {
+        'region': '',
+        'latitude': 0.0,
+        'longitude': 0.0,
+    }
+]
+
+
+def find_closest_gs(datacenter):
+    """
+    Find the closest ground station to the given datacenter.
+    @param datacenter: the datacenter
+    @return: the ID of the closest ground station
+    """
+    pass
