71adcfcef34bc80d275de7571a895bca113c2c46
Yi Ching Chou (David) <mrycchou@gmail.com>
2022-07-13 16:09:17 -0700

    Add datacenter lat lon

--- integration_tests/infocom-test/tcp/datacenter.py
+++ integration_tests/infocom-test/tcp/datacenter.py
@@ -1,9 +1,44 @@
 DATACENTERS = [
     {
-        'region': '',
-        'latitude': 0.0,
-        'longitude': 0.0,
-    }
+        'region': 'Bahrain',
+        'latitude': 26.061511,
+        'longitude': 50.534737,
+    },
+    {
+        'region': 'Cape Town',
+        'latitude': -33.926226,
+        'longitude': 18.420863,
+    },
+    {
+        'region': 'London',
+        'latitude': 51.502339,
+        'longitude': -0.058766,
+    },
+    {
+        'region': 'Mumbai',
+        'latitude': 19.050027,
+        'longitude': 72.883578,
+    },
+    {
+        'region': 'North California',
+        'latitude': 40.760744,
+        'longitude': -123.291029,
+    },
+    {
+        'region': 'Sao Paulo',
+        'latitude': 1.344019,
+        'longitude': 103.855261,
+    },
+    {
+        'region': 'Sydney',
+        'latitude': -33.877200,
+        'longitude': 151.220916,
+    },
+    {
+        'region': 'Tokyo',
+        'latitude': 35.696570,
+        'longitude': 139.817398,
+    },
 ]
 
 
