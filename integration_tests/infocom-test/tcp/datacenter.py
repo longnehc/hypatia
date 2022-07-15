84c4ff9ef55240e448486da89c4646b9389324b1
Yi Ching Chou (David) <mrycchou@gmail.com>
2022-07-14 17:20:30 -0700

    Add more AWS datacenters

--- integration_tests/infocom-test/tcp/datacenter.py
+++ integration_tests/infocom-test/tcp/datacenter.py
@@ -44,6 +44,96 @@ DATACENTERS = [
         'latitude': 35.696570,
         'longitude': 139.817398,
     },
+    {
+        'region': 'Oregon',
+        'latitude': 45.006884,
+        'longitude': -121.477004,
+    },
+    {
+        'region': 'AWS GovCloud (US-West)',
+        'latitude': 42.262954,
+        'longitude': -118.298180,
+    },
+    {
+        'region': 'Ohio',
+        'latitude': 40.154962,
+        'longitude': -82.860364,
+    },
+    {
+        'region': 'AWS GovCloud (US-East)',
+        'latitude': 39.022142,
+        'longitude': -78.153935,
+    },
+    {
+        'region': 'Northern Virginia',
+        'latitude': 39.082352,
+        'longitude': -77.697201,
+    },
+    {
+        'region': 'Canada Central',
+        'latitude': 46.553088,
+        'longitude': -76.491743,
+    },
+    {
+        'region': 'Ireland',
+        'latitude': 53.224383,
+        'longitude': -7.474220,
+    },
+    {
+        'region': 'Paris',
+        'latitude': 48.877008,
+        'longitude': 2.371268,
+    },
+    {
+        'region': 'Frankfurt',
+        'latitude': 50.089564,
+        'longitude': 8.637410,
+    },
+    {
+        'region': 'Milan',
+        'latitude': 45.448918,
+        'longitude': 9.195937,
+    },
+    {
+        'region': 'Europe (Stockholm)',
+        'latitude': 63.719112,
+        'longitude': 14.202794,
+    },
+    {
+        'region': 'Ningxia',
+        'latitude': 37.251822,
+        'longitude': 105.880336,
+    },
+    {
+        'region': 'Beijing',
+        'latitude': 39.858346,
+        'longitude': 116.397592,
+    },
+    {
+        'region': 'Seoul',
+        'latitude': 37.532198,
+        'longitude': 126.998110,
+    },
+    {
+        'region': 'Osaka',
+        'latitude': 34.674190,
+        'longitude': 135.516591,
+    },
+    {
+        'region': 'Hong Kong SAR',
+        'latitude': 22.327122,
+        'longitude': 114.181792,
+    },
+    {
+        'region': 'Singapore',
+        'latitude': 1.340082,
+        'longitude': 103.869646,
+    },
+    {
+        'region': 'Jakarta',
+        'latitude': -6.191501,
+        'longitude': 106.833628,
+    },
 ]
 
 
