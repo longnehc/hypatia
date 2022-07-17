5b3ff01ea08489655cba5ee5b9f3fe60f56e4b43
Yi Ching Chou (David) <mrycchou@gmail.com>
2022-07-17 14:17:08 -0700

    Count non zero progress flows

--- integration_tests/infocom-test/util/analyze_flow_result.py
+++ integration_tests/infocom-test/util/analyze_flow_result.py
@@ -18,6 +18,26 @@ def count_successful_flows(filepath):
     return counts
 
 
+def count_non_zero_progress_flows(filepath):
+    total = 0
+    counts = 0
+    unique_total_pairs = set()
+    unique_non_zero_pairs = set()
+    with open(filepath, 'r') as f:
+        for line in f:
+            total += 1
+            split = line.strip().split(',')
+            unique_total_pairs.add((split[1], split[2]))
+            if int(split[7]) != 0:
+                counts += 1
+                unique_non_zero_pairs.add((split[1], split[2]))
+    print('{:.2f}% non-zero progress flows, {} non-zero progress flows out of {} total flows.'
+          .format(counts / total * 100.0, counts, total))
+    print('{} unique non-zero progress pairs out of {} unique total pairs.'
+          .format(len(unique_non_zero_pairs), len(unique_total_pairs)))
+    return counts
+
+
 def output_successful_flow_end_point_pairs(filepath, output_path):
     """
     Go through the tcp_flows.csv and output the success flows
@@ -42,8 +62,10 @@ def output_successful_flow_end_point_pairs(filepath, output_path):
 
 
 if __name__ == '__main__':
-    count_successful_flows('../temp/runs/starlink_550_isls_none_tcp/logs_ns3/tcp_flows.csv')
+    tcp_csv_path = '../temp/runs/starlink_550_isls_none_tcp/logs_ns3/tcp_flows.csv'
+    count_non_zero_progress_flows(tcp_csv_path)
+    count_successful_flows(tcp_csv_path)
     output_successful_flow_end_point_pairs(
-        filepath='../temp/runs/starlink_550_isls_none_tcp/logs_ns3/tcp_flows.csv',
+        filepath=tcp_csv_path,
         output_path='../ep_pairs_cache.txt',
     )
