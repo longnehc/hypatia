8da1cd0cf141d0f596aca4ff614419391775900e
Yi Ching Chou (David) <mrycchou@gmail.com>
2022-07-17 00:08:39 -0700

    Count unique pairs

--- integration_tests/infocom-test/util/analyze_flow_result.py
+++ integration_tests/infocom-test/util/analyze_flow_result.py
@@ -1,14 +1,20 @@
 def count_successful_flows(filepath):
     total = 0
     counts = 0
+    unique_total_pairs = set()
+    unique_yes_pairs = set()
     with open(filepath, 'r') as f:
         for line in f:
             total += 1
             split = line.strip().split(',')
+            unique_total_pairs.add((split[1], split[2]))
             if split[8] == 'YES':
                 counts += 1
+                unique_yes_pairs.add((split[1], split[2]))
     print('{:.2f}% YES flows, {} YES flows out of {} total flows.'
           .format(counts / total * 100.0, counts, total))
+    print('{} unique yes pairs out of {} unique total pairs.'
+          .format(len(unique_yes_pairs), len(unique_total_pairs)))
     return counts
 
 
@@ -19,27 +25,34 @@ def output_successful_flow_end_point_pairs(filepath, output_path):
     """
     # Read flow file
     total = 0
-    pairs_set = set()
+    counts = 0
+    unique_total_pairs = set()
+    unique_yes_pairs = set()
     with open(filepath, 'r') as f:
         for line in f:
             total += 1
             split = line.strip().split(',')
+            unique_total_pairs.add((split[1], split[2]))
             if split[8] == 'YES':
-                pairs_set.add(
+                counts += 1
+                unique_yes_pairs.add(
                     (split[1], split[2])
                 )
     # Write successful end-point pairs
-    pairs_list = list(pairs_set)
+    pairs_list = list(unique_yes_pairs)
     pairs_list.sort()
     with open(output_path, 'w') as f:
         for p in pairs_list:
             f.write('{}, {}\n'.format(p[0], p[1]))
 
     print('{:.2f}% YES flows, {} YES flows out of {} total flows.'
-          .format(len(pairs_list) / total * 100.0, len(pairs_list), total))
+          .format(counts / total * 100.0, counts, total))
+    print('{} unique yes pairs out of {} unique total pairs.'
+          .format(len(unique_yes_pairs), len(unique_total_pairs)))
 
 
 if __name__ == '__main__':
+    # count_successful_flows('../temp/runs/starlink_550_isls_none_tcp/logs_ns3/tcp_flows.csv')
     output_successful_flow_end_point_pairs(
         filepath='../temp/runs/starlink_550_isls_none_tcp/logs_ns3/tcp_flows.csv',
         output_path='../ep_pairs_cache.txt',
